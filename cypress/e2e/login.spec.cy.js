395c3a3d4fcc383f25a2523d53111d4fe072c370
Raquel Frota <raquelfrota@gmail.com>
2026-01-05 20:57:54 -0300

    base url added

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -22,7 +22,7 @@ describe('Orange HRM tests', () => {
   }
 
   it('Login c- Success', () => {
-    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.visit('/auth/login')
     cy.get(selectorsList.usernameField).type(userData.userSuccess.username)
     cy.get(selectorsList.passwordField).type(userData.userSuccess.password)
     cy.get(selectorsList.loginButton).click()
@@ -30,7 +30,7 @@ describe('Orange HRM tests', () => {
     cy.get(selectorsList.sectionTitleTopBar).contains('Dashboard')
   })
   it('Login c- Fail', () => {
-    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.visit('/auth/login')
     cy.get(selectorsList.usernameField).type(userData.userFail.username)
     cy.get(selectorsList.passwordField).type(userData.userFail.password)
     cy.get(selectorsList.loginButton).click()
